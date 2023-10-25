12320a5bb050c45d189cc625e21c3a0b55455211
Денис Громогласов <dgromoglasov@nsk.beeline.ru>
2023-10-25 18:41:54 +0700

    YC: bastion+VPN

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # denis-54_infra
 denis-54 Infra repository
+
+Первая часть задания:
+
+Использование bastion для доступа к хосту во внетренней сети:
+ssh -J appuser@51.250.75.102 appuser@10.128.0.32
+    где -J -опция указывающая на возможность использовать промежуточный хост
+    51.250.75.102 - внешний ip-адрес бастион-зоста
+    10.128.0.32 - внетренний ip-адрес someinternalhost
+
+Второй вариант:
+ssh -o ProxyCommand="ssh -W %h:%p appuser@51.250.75.102" appuser@10.128.0.32
+
+Третий вариант (c алиасом):
+    в ~/.ssh/config добавляем:
+
+Host someinternalhost
+    HostName 10.128.0.32
+    User appuser
+    ProxyCommand ssh -W %h:%p appuser@51.250.75.102
+
+Команда подключения в данном случае будет выглядеть так:
+
+ssh someinternalhost
+
+Вторая часть задания:
+
+bastion_IP = 158.160.120.193
+someinternalhost_IP = 10.128.0.32
---
+++ setupvpn.sh
@@ -0,0 +1,44 @@
+sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo << EOF
+[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+EOF
+
+sudo tee /etc/yum.repos.d/pritunl.repo << EOF
+[pritunl]
+name=Pritunl Repository
+baseurl=https://repo.pritunl.com/stable/yum/oraclelinux/8/
+gpgcheck=1
+enabled=1
+EOF
+
+# Oracle Linux only
+sudo yum -y install oracle-epel-release-el8
+sudo yum-config-manager --enable ol8_developer_EPEL
+# AlmaLinux/Rocky Linux/RHEL
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+sudo yum -y update
+
+# WireGuard server support
+sudo yum -y install wireguard-tools
+
+sudo yum -y remove iptables-services
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
+
+# Import signing key from keyserver
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
+# Alternative import from download if keyserver offline
+sudo rpm --import https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc
+
+# Install updated openvpn package from pritunl
+sudo yum --allowerasing install pritunl-openvpn
+
+sudo yum -y install pritunl mongodb-org
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
