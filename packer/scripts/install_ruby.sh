dccd28ffeb6a007c0194c87d0873d755091d1d43
Денис Громогласов <dgromoglasov@nsk.beeline.ru>
2024-01-31 13:51:32 +0700

    Ansible-2

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-echo Waiting for apt-get to finish...
-a=1; while [ -n "$(pgrep apt-get)" ]; do echo $a; sleep 1s; a=$(expr $a + 1); done
-sudo apt install -y ruby-full ruby-bundler build-essential
-
+until apt update 2>&1;
+do
+  sleep 1
+done
+apt install -y ruby-full ruby-bundler build-essential
